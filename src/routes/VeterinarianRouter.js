c9dd3222de7a89f608985694c503fa0bce99118f
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-10-26 16:41:20 -0300

    Controller de consultation concluído, arrumando as rotas de veterinários

--- src/routes/VeterinarianRouter.js
+++ src/routes/VeterinarianRouter.js
@@ -6,10 +6,11 @@ router.post("/veterinarian/new")
 router.get("/veterinarian/find/all", checkToken)
 router.get("/veterinarian/find/:veterinarian_id", checkToken, )
 router.get("/veterinarian/find/veterinarian_name", checkToken, )
-router.get("/veterinarian/find/consultation/today", checkToken, )
-router.get("/veterinarian/find/consultation/all", checkToken)
-router.get("/users/find/all/:veterinarian_id", checkToken, )
+router.get("/veterinarian/find/consultation/today/:veterinarian_id", checkToken, )
+router.get("/veterinarian/find/consultation/all/:veterinarian_id", checkToken, )
 
 router.put("/veterinarian/alter/:veterinarian_id", checkToken, )
+
+router.delete("/veterinarian/delete/:veterinarian_id", checkToken, )
 
 module.exports = router
